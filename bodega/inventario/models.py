7573696b201f527c08057c711e1be821ef2586d9
sixto69 <sixto.obreque@gmail.com>
2025-03-09 17:59:55 -0300

    se crea otro codigo

--- bodega/inventario/models.py
+++ bodega/inventario/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 
 class Producto(models.Model):
+    id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
     cantidad = models.IntegerField()
     fecha_ingreso = models.DateField(auto_now_add=True)
 
     def __str__(self):
-        return self.nombre
+        return f"{self.id} - {self.nombre}"  # Muestra ID en el nombre
 
 # Create your models here.
